1020fa51e936fb2a868af7c42fb68da9505e62ca
yiannisha <hadjiyiannis.3@gmail.com>
2024-04-20 12:21:20 +0300

    Update example to test new functionality

--- examples/show_video.py
+++ examples/show_video.py
@@ -1,11 +1,13 @@
 import cv2
-from src.video import Video
+from videolib.video import Video
 
 video = Video()
 
 while True:
   try:
-    img = video.get_frame_nparr()
+    img, bytes, fstring = video.get_frame_nparr(_return_string=True, _return_bytes=True)
+    print(type(fstring))
+    print(type(bytes))
     cv2.imshow("feed", img)
     cv2.waitKey(1)
   except KeyboardInterrupt:
